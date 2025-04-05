c3e4f9be2e71c1f88bd51f80636643107c5ad9ac
catcherd <76436995+catcherd@users.noreply.github.com>
2025-04-05 19:35:56 +0800

    2.1.3 version

---
+++ zigbee2mqtt-2.1.3/update.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+
+NEED_RESTART=0
+
+OSNAME="$(uname -s)"
+if [ "$OSNAME" == "FreeBSD" ]; then
+    echo "Checking Zigbee2MQTT status..."
+    if service zigbee2mqtt status >/dev/null; then
+        echo "Stopping Zigbee2MQTT..."
+        service zigbee2mqtt stop
+        NEED_RESTART=1
+    fi
+else
+    if which systemctl 2> /dev/null > /dev/null; then
+        echo "Checking Zigbee2MQTT status..."
+        if systemctl is-active --quiet zigbee2mqtt; then
+            echo "Stopping Zigbee2MQTT..."
+            sudo systemctl stop zigbee2mqtt
+            NEED_RESTART=1
+        fi
+    else
+        echo "Skipped stopping Zigbee2MQTT, no systemctl found"
+    fi
+fi
+
+echo "Updating..."
+git pull --no-rebase
+
+echo "Installing dependencies..."
+pnpm i --frozen-lockfile
+
+echo "Building..."
+pnpm run build
+
+if [ $NEED_RESTART -eq 1 ]; then
+    echo "Starting Zigbee2MQTT..."
+    if [ "$OSNAME" == "FreeBSD" ]; then
+        service zigbee2mqtt start
+    else
+        sudo systemctl start zigbee2mqtt
+    fi
+fi
+
+echo "Done!"
